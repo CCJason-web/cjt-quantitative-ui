ea970731896bc8deb26aa55aee6f73187e556d16
githubCJT <pkjin1013@gmail.com>
2021-04-05 13:34:26 +0800

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './css/index.css';
+import App from './components/App';
+import './css/index.css';
+// import * as serviceWorker from './serviceWorker';
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/umd/popper.js';
+import { Footer } from './components/layout/Footer';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App props/>
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// serviceWorker.unregister();
---
+++ src/setupProxy.js
@@ -0,0 +1,10 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use('/quantity-search',
+    createProxyMiddleware({
+      target: 'http://localhost:8081',
+      changeOrigin: true,
+    })
+  );
+};
